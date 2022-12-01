27d7bf6acec31ae3e7bcd7ba4580ebb235d780e5
Amaury Soto Garay <amaury.soto@globant.com>
2022-12-01 12:29:41 -0500

    IMPORTANT created models instance

---
+++ lib/models/models.dart
@@ -0,0 +1,3 @@
+export 'package:fl_movies_app/models/movie.dart';
+export 'package:fl_movies_app/models/now_playing_response.dart';
+
--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,14 +1,34 @@
+import 'dart:convert';
+
+import 'package:fl_movies_app/models/now_playing_response.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 //instancia movies_provider
 //Necesita extender de ChangeNotifier
+
 class MoviesProvider extends ChangeNotifier {
+  String _apiKey = '78fdfca86cfd9c3de43463f57599da37';
+  String _baseUrl = 'api.themoviedb.org';
+  String _language = 'es-ES';
+
   MoviesProvider() {
     print('movies provider started...');
     //cuando el constructor sea llamado, mandamos a ejecutar los siguientes métodos
     getOnDisplayMovies();
   }
+
   //metodo 1
-  getOnDisplayMovies() {
-    print('get on display movies');
+  getOnDisplayMovies() async {
+    var url = Uri.https(_baseUrl, '3/movie/now_playing', {
+      'api_key': _apiKey,
+      'language': _language,
+      'page': '1',
+    });
+
+    final response = await http.get(url);
+    final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+    final Map<String, dynamic> decodeData = json.decode(response.body);
+    print(nowPlayingResponse.results[1].title);
   }
 }
